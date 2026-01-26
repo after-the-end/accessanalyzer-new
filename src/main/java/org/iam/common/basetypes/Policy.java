354e0311bdc26200ee5b36ef7cd327c062543a63
jyzhang <jyzhang0281@foxmail.com>
2026-01-26 08:33:30 +0800

    Update new Policy which has its cachedExpr

--- src/main/java/org/iam/common/basetypes/Policy.java
+++ src/main/java/org/iam/common/basetypes/Policy.java
@@ -1,32 +1,38 @@
 package org.iam.common.basetypes;
 
 import com.fasterxml.jackson.annotation.*;
+import org.iam.common.apis.EncodedAPI;
+import org.iam.common.apis.GrammarlyAPI;
+import org.iam.common.vars.VarEffect;
+import org.iam.common.vars.VarKey;
 import org.iam.utils.PolicyParser;
 
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
-public class Policy {
-
-    @JsonIgnore
-    protected String id;
+public class Policy<T> implements GrammarlyAPI<T> {
 
     @JsonProperty("Version")
     protected String version;
 
     @JsonProperty("Statement")
-    protected Set<Statement> statement;
+    protected Set<Statement<T>> statements;
 
-    public Policy() {}
+    @JsonIgnore
+    private Map<VarKey, Set<String>> kvMap = null;
 
-    public Policy(Policy other) {
+    @JsonIgnore
+    private T encodedExpr = null;
+
+    public Policy(Policy<T> other) {
         this.version = other.version;
-        this.statement = other.statement;
+        this.statements = other.statements;
+        this.kvMap = other.kvMap;
+        this.encodedExpr = other.encodedExpr;
     }
 
-    public Policy(String version, Set<Statement> statement) {
+    public Policy(String version, Set<Statement<T>> statement) {
         this.version = version;
-        this.statement = statement;
+        this.statements = statement;
     }
 
     public String getVersion() {
@@ -37,30 +43,78 @@ public class Policy {
         this.version = version;
     }
 
-    public Set<Statement> getStatement() {
-        return statement;
+    public Set<Statement<T>> getStatements() {
+        return statements;
     }
 
-    public void setStatement(Set<Statement> statement) {
-        this.statement = statement;
+    public void setStatements(Set<Statement<T>> statements) {
+        this.statements = statements;
+    }
+
+    public Map<VarKey, Set<String>> getKvMap() {
+        if (this.kvMap == null) {
+            this.kvMap = buildKvMap();
+        }
+        return this.kvMap;
+    }
+
+    public Set<VarKey> keySet() {
+        return this.getKvMap().keySet();
+    }
+
+    private Map<VarKey, Set<String>> buildKvMap() {
+        Map<VarKey, Set<String>> newKvMap = new HashMap<>();
+        for (Statement<T> statement : this.statements) {
+            Map<VarKey, Set<String>> principals = statement.getPrincipal();
+            for (VarKey key : principals.keySet()) {
+                kvMap.computeIfAbsent(key, k -> new HashSet<>()).addAll(principals.get(key));
+            }
+            kvMap.computeIfAbsent(VarKey.ACTION, k -> new HashSet<>()).addAll(statement.getAction());
+            kvMap.computeIfAbsent(VarKey.RESOURCE, k -> new HashSet<>()).addAll(statement.getResource());
+            for (Condition<T> condition : statement.getCondition()) {
+                kvMap.computeIfAbsent(condition.getKey(), k -> new HashSet<>()).addAll(condition.getValues());
+            }
+        }
+        return kvMap;
     }
 
     @Override
-    public String toString() {
-        return PolicyParser.toString(this);
+    public T encode(EncodedAPI<T> helper) {
+        if (this.statements == null || this.statements.isEmpty()) {
+            throw new IllegalStateException("No valid statements in Policy, a parsing error may occurred.");
+        }
+
+        if (this.encodedExpr == null) {
+            List<T> allowExprs = statements.stream()
+                    .filter(s -> s.getEffect() == VarEffect.Allow)
+                    .map(s -> s.encode(helper))
+                    .toList();
+            List<T> denyExprs = statements.stream()
+                    .filter(s -> s.getEffect() == VarEffect.Deny)
+                    .map(s -> s.encode(helper))
+                    .toList();
+            // allowed = allow1 OR allow2 OR ...
+            // denied = deny1 OR deny2 OR ...
+            // allowed AND NOT denied
+            this.encodedExpr = helper.and(List.of(
+                    helper.or(allowExprs),
+                    helper.not(helper.or(denyExprs))
+            ));
+        }
+
+        return this.encodedExpr;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Policy policy = (Policy) o;
-        return Objects.equals(version, policy.version) && Objects.equals(statement, policy.statement);
+        if (!(o instanceof Policy<?> policy)) return false;
+        return Objects.equals(version, policy.version) && Objects.equals(statements, policy.statements);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(version, statement);
+        return Objects.hash(version, statements);
     }
 }
 
