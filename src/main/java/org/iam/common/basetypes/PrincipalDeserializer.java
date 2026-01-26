752b7ed4e8769fb7f76e32f1dcd237d439926889
jyzhang <jyzhang0281@foxmail.com>
2026-01-26 10:08:00 +0800

    Change principal model - Use Map<VarKey, Set<String>> instead of Principal class - Have modified PrincipalDeserializer and will delete Principal.java in a moment

--- src/main/java/org/iam/common/basetypes/PrincipalDeserializer.java
+++ src/main/java/org/iam/common/basetypes/PrincipalDeserializer.java
@@ -1,41 +1,47 @@
 package org.iam.common.basetypes;
 
+import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
+import org.iam.common.vars.VarKey;
 
 import java.io.IOException;
 import java.util.*;
 
-public class PrincipalDeserializer extends JsonDeserializer<Set<Principal>> {
+public class PrincipalDeserializer extends JsonDeserializer<Map<VarKey, Set<String>>> {
 
     @Override
-    public Set<Principal> deserialize(JsonParser jp, DeserializationContext context)
-            throws IOException {
+    public Map<VarKey, Set<String>> deserialize(JsonParser jp, DeserializationContext context)
+            throws IOException, JacksonException {
         JsonNode node = jp.getCodec().readTree(jp);
-        Set<Principal> principals = new HashSet<>();
+        Map<VarKey, Set<String>> principals = new HashMap<>();
+
         Iterator<Map.Entry<String, JsonNode>> fields = node.fields();
 
         while (fields.hasNext()) {
             Map.Entry<String, JsonNode> field = fields.next();
-            String prpKey = field.getKey();
+            VarKey prpKey = VarKey.fromString(field.getKey());
             Set<String> values = new HashSet<>();
 
             JsonNode arrayNode = field.getValue();
 
             if (arrayNode.isArray()) {
                 for (JsonNode valueNode : arrayNode) {
-                    values.add(valueNode.asText());
+                    values.add(formatRegex(valueNode.asText()));
                 }
-            } else if (arrayNode.isTextual()) {
-                values.add(arrayNode.asText());
             } else {
-                throw new IOException("Invalid principal value: " + field.getValue());
+                values.add(formatRegex(arrayNode.asText()));
             }
 
-            principals.add(new Principal(prpKey, values));
+            principals.put(prpKey, values);
         }
         return principals;
+    }
+
+    private String formatRegex(String source) {
+        if (source == null) return null;
+        return source.replace("*", ".*").replace("?", ".");
     }
 }
