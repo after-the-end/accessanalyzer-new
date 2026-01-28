e1ea0ee85f257b795dea0443e090bb687dc1313b
jyzhang <jyzhang0281@foxmail.com>
2026-01-29 07:39:47 +0800

    Complement 'refine' and 'reduce' methods.

--- src/main/java/org/iam/common/basetypes/Finding.java
+++ src/main/java/org/iam/common/basetypes/Finding.java
@@ -3,6 +3,7 @@ package org.iam.common.basetypes;
 import org.iam.common.apis.EncodedAPI;
 import org.iam.common.apis.GrammarlyAPI;
 import org.iam.common.vars.VarKey;
+import org.iam.core.KvRelations;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -32,6 +33,48 @@ public class Finding<T> implements GrammarlyAPI<T> {
 
     public Map<VarKey, String> getFinding() {
         return finding;
+    }
+
+    public T reduce(KvRelations relations, EncodedAPI<T> helper) {
+        List<T> keyIdomExprs = this.finding.entrySet().stream().map(
+                entry -> {
+                    VarKey key = entry.getKey();
+                    String value = entry.getValue();
+                    Set<String> idomValues = relations.idom(key, value);
+                    T idomExpr;
+                    if (idomValues == null || idomValues.isEmpty()) {
+                        idomExpr = null;
+                    } else {
+                        List<T> idomExprs = idomValues.stream().map(
+                                idomValue -> {
+                                    if (idomValue == null) {
+                                        return helper.mkFalse();
+                                    }
+                                    return helper.mkReMatch(key.toString(), idomValue);
+                                }).toList();
+                        idomExpr = helper.or(idomExprs);
+                    }
+                    T reducedExpr = helper.mkReMatch(key.toString(), value);
+                    if (idomExpr != null) {
+                        reducedExpr = helper.and(reducedExpr, helper.not(idomExpr));
+                    }
+                    return reducedExpr;
+                }).toList();
+        return helper.and(keyIdomExprs);
+    }
+
+    public Set<Finding<T>> refine(KvRelations relations) {
+        Set<Finding<T>> refinements = new HashSet<>();
+        for (VarKey key : this.finding.keySet()) {
+            Set<String> values = relations.idom(key, this.finding.get(key));
+            for (String idomValue : values) {
+                Map<VarKey, String> refinedFinding = new HashMap<>(this.finding);
+                refinedFinding.put(key, idomValue);
+                Finding<T> newFinding = new Finding<>(refinedFinding);
+                refinements.add(newFinding);
+            }
+        }
+        return refinements;
     }
 
     @Override
